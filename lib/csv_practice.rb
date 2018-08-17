d2bee86b9739fa899dc1993035c675a6114e4245
Chantelle Belic <chantelle.belic@gmail.com>
2018-08-17 14:33:00 -0700

    first load_data(filename) working

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,15 +1,39 @@
 # csv_practice.rb
 require 'csv'
 require 'awesome_print'
+require 'pry'
 
 # Part 1 - CSV Practice
-def load_data(filename)
 
+def load_data(filename)
+  #array of hashes saved as csv object
+  #each row corresponds to athlete and their data is the hash key and values
+
+  athlete_data = CSV.read(filename, headers: true).map do |athlete|
+    athlete.to_hash
+  end
+# binding.pry
+  return athlete_data
 end
+
+#
+# p load_data('data/athlete_events.csv')
 
 def total_medals_per_country(olympic_data)
 
+team_data = []
+
+  #total_medals_per_country(olympic_data) - This method takes the array of hashes read in from the file in load_data and returns an array of hashes with each element having a country name (team), and number of medals that country (team) has won. The "Medal" header for the data indicates if the athlete won ('Gold', 'Silver' 'Bronze') or not ('NA').
+
+  athlete_data = load_data(olympic_data)
+
+  #return team_data
+  #[{team: , num_metals_won: },
+# => ]
+
 end
+
+total_medals_per_country('data/athlete_events.csv')
 
 def save_medal_totals(filename, medal_totals)
 
