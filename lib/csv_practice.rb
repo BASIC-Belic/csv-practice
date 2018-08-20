93a22905b0d845ea83763aa8aec7470c00d22018
Chantelle Belic <chantelle.belic@gmail.com>
2018-08-19 17:44:08 -0700

    save_medal_totals working, with headers country, total_medals as first row

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -5,9 +5,10 @@ require 'pry'
 
 # Part 1 - CSV Practice
 
+#array of hashes saved as csv object
+#each row corresponds to athlete and their data is the hash key and values
 def load_data(filename)
-  #array of hashes saved as csv object
-  #each row corresponds to athlete and their data is the hash key and values
+
 
   athlete_data = CSV.read(filename, headers: true).map do |athlete|
     athlete.to_hash
@@ -16,52 +17,51 @@ def load_data(filename)
   return athlete_data
 end
 
-#TEST:
-# p load_data('data/athlete_events.csv')
-
+#returns array of hashes, team medal totals and team names ad keys
 def total_medals_per_country(olympic_data)
 
-
-  #  athlete_data = load_data(olympic_data)
-
+  #group data by Team
+  #returns hash where key is Team name, with array of hashes embedded
   current_team_data = olympic_data.group_by do |athlete|
     athlete["Team"]
 
   end
 
+  #iterate through each key of Team/country and delete items in the array of athlete hashes where medal is equal is equal to NA
   current_team_data.each do |country_name, athletes_array|
     athletes_array.delete_if do |athlete|
       athlete["Medal"] == "NA"
     end
-
   end
-  # binding.pry
 
+  #maps through team/country hash to create new array of hashes, where keys are country name and total medals
   current_team_data.map do |country_name, athletes_array|
     {
       country: country_name,
       total_medals: athletes_array.length
     }
-    #return country name and length
   end
-
-  #SAME AS ABOVE
-  # team_data  =[]
-  # current_team_data.each do |country_name, athletes_array|
-  #   temporary_hash = {
-  #     country: country_name,
-  #     total_medals: athletes_array.length
-  #   }
-  #   team_data << temporary_hash
-  #
-  #   #return country name and length
-  # end
 end
 
 # total_medals_per_country('data/athlete_events.csv')
 
 def save_medal_totals(filename, medal_totals)
 
+  # This method writes the medal total information returned from total_medals_per_country to another CSV file provided by filename.
+
+  #we have medal_totals array of hashes [{ :country , :totals}, ]
+  #open and write medal_totals to a new csv file
+  CSV.open(filename, "wb") do |row|
+
+    row  << ["country", "total_medals"]
+
+    medal_totals.each do |country|
+
+      row << [country[:country], country[:total_medals]]
+      ap "#{country}"
+
+    end
+  end
 end
 
 # Part 2 - More Enumerable Practice
